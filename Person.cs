16b31a093f9eccc9b439e9f33e05454c3abc5da9
Ethan <enephew2@mail.valenciacollege.edu>
2021-07-28 10:54:04 -0400

    Uploading OOP example.

---
+++ Person.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/*
+ * This is the base or parent class of the Chapter10_Student class. 
+ * Refer to Chapter10_Student for an example of inheritance. 
+ */
+namespace Chapter10_PersonStudent
+{
+    class Person // Example 10-1, pg 582
+    {
+        private string idNumber;
+        private string lastName;
+        private string firstName;
+        private int age;
+
+        public string IDNumber
+        {
+            get
+            {
+                return idNumber;
+            }
+            set
+            {
+                idNumber = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+        public Person()
+        {
+            idNumber = "";
+            lastName = "unknown";
+            firstName = string.Empty;
+            age = 0;
+        }
+        public Person(string id)
+        {
+            idNumber = id;
+            lastName = "unknown";
+            firstName = string.Empty;
+            age = 0;
+        }
+        public Person(string id, string lname, string fname)
+        {
+            idNumber = id;
+            lastName = lname;
+            firstName = fname;
+            age = 0;
+        }
+        public Person(string id, string lname, string fname, int anAge)
+        {
+            idNumber = id;
+            lastName = lname;
+            firstName = fname;
+            age = anAge;
+        }
+        public override string ToString()
+        {
+            /*
+             * In the object class, the ToString method looks like this:
+             * public virtual string ToString();
+             */
+            return firstName + " " + lastName;
+        }
+        public virtual int GetSleepAmt() // Virtual means that the method can be overridden in the child class. 
+        {
+            return 8;
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/*
+ * This is an example of how inheritance syntax is layed out in C#. 
+ * Java uses the super keyword, but C# uses base. 
+ * 
+ * The DLL is built to the following directory:
+ * C:\Users\Ethan\source\repos\Chapter10_PersonStudent\bin\Debug\netcoreapp3.1\Chapter10_PersonStudent.dll
+ */
+namespace Chapter10_PersonStudent
+{
+    class Student : Person // Student is derrived from Person, a Student IS A Person.
+    {
+        private string major;
+        private int studentId;
+
+        public string Major
+        {
+            get
+            {
+                return major;
+            }
+            set
+            {
+                major = value;
+            }
+        }
+        public int StudentId
+        {
+            get
+            {
+                return studentId;
+            }
+            set
+            {
+                studentId = value;
+            }
+        }
+
+        public Student() : base() // This means that no arguments being sent to base() constructor. 
+        {
+            major = "unknown";
+            studentId = 0;
+        }
+        public Student(string id, string fname, string lname, string maj, int sId) : base(id, lname, fname)
+        {
+            /*
+             * id, lname, and fname are sent to the base 3-param constructor. 
+             */
+            major = maj;
+            studentId = sId;
+        }
+        public override int GetSleepAmt()
+        {
+            return 6;
+        }
+        public int CallOverriddenGetSleepAmt()
+        {
+            /*
+             * If you wish to override a base method, but still want to have access to it, 
+             * then this is a good way of doing it. Create a method that calls the base method. 
+             */
+            return base.GetSleepAmt();
+        }
+    }
+}
+
